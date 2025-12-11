f1c98f659bee98c74cd9434d29f6a019b3a56874
fl4tisjustice <chaoticflat@gmail.com>
2025-12-11 18:17:52 +0000

    Better typing info

--- day-10/day-10.py
+++ day-10/day-10.py
@@ -13,7 +13,7 @@ def get_input() -> list[tuple[int, tuple[int, ...], tuple[int, ...]]]:
     ret : list[tuple[int, tuple[int, ...], tuple[int, ...]]] = []
     for line in raw:
         lights_raw = cast(re.Match[str], re.search(r"(?<=\[)[.#]+(?=\])", line)).group()
-        buttons_raw : list[str] = list(map(re.Match.group, re.finditer(r"(?<=\()([0-9]+,)*[0-9]+(?=\))", line)))
+        buttons_raw : list[str] = list(map(re.Match[str].group, re.finditer(r"(?<=\()([0-9]+,)*[0-9]+(?=\))", line)))
         joltages : tuple[int, ...] = tuple(map(int, cast(re.Match[str], re.search(r"(?<=\{)([0-9]+,)*[0-9]+(?=})", line)).group().split(",")))
 
         lights : int = reduce(lambda accum, curr : (accum << 1) | int(curr == "#"), lights_raw, 0)
@@ -37,11 +37,11 @@ def part_one(puzzle : list[tuple[int, tuple[int, ...], tuple[int, ...]]]) -> int
                 queue.extend(map(lambda button : (step + 1, state ^ button), buttons))
     return total_min_steps
 
-def part_two(puzzle : list[tuple[int, tuple[int, ...]]]) -> int:
+def part_two(puzzle : list[tuple[int, tuple[int, ...], tuple[int, ...]]]) -> int:
     total_min_steps : float = 0.0
     for _, buttons, joltages in puzzle:
-        A : list[tuple[int, ...]] = [[int(((1 << i) & button) != 0) for button in buttons] for i in range(len(joltages) - 1, -1, -1)]
-        total_min_steps += sp.optimize.linprog([1] * len(buttons), A_eq = A, b_eq = joltages, integrality = 1).fun
+        A : list[list[int]] = [[int(((1 << i) & button) != 0) for button in buttons] for i in range(len(joltages) - 1, -1, -1)]
+        total_min_steps += cast(float, sp.optimize.linprog([1] * len(buttons), A_eq = A, b_eq = joltages, integrality = 1).fun)
     return int(total_min_steps)
 
 def main() -> None:
